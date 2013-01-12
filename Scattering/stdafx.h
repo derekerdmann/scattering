55171901caee5e37d754bb5e3c45b563a5684752
Derek Erdmann <derek@derekerdmann.com>
2013-01-12 18:07:59 -0500

    Add DirectX headers to precompiled header

--- Scattering/stdafx.h
+++ Scattering/stdafx.h
@@ -9,6 +9,6 @@
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
 
-
-// TODO: reference additional headers your program requires here
