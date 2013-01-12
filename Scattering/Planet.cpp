218737423ce4a869c29fc69f2f5c8b3da0ab95cd
Derek Erdmann <derek@derekerdmann.com>
2013-01-12 18:08:20 -0500

    Add empty Planet class

---
+++ Scattering/Planet.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include "Planet.h"
+
+using namespace DirectX;
+using namespace Scattering;
+
+Planet::Planet(void)
+{
+}
+
+
+Planet::~Planet(void)
+{
+}
---
+++ Scattering/Planet.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace Scattering {
+
+/* Represents a planet with an atmosphere */
+class Planet {
+public:
+
+    Planet(void);
+    virtual ~Planet(void);
+};
+
+}
