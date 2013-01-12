d539cb3996fd83cc15412289627bde4a8cf1c327
Derek Erdmann <derek@derekerdmann.com>
2013-01-12 18:18:45 -0500

    Start defining Planet members

--- Scattering/Planet.h
+++ Scattering/Planet.h
@@ -1,13 +1,32 @@
 #pragma once
 
+#include "Atmosphere.h"
+
 namespace Scattering {
 
 /* Represents a planet with an atmosphere */
 class Planet {
 public:
 
-    Planet(void);
+    /* Constructor
+     * radius - The radius of the planet
+     * karmanLine - The upper boundary of the planet's atmosphere
+     */
+    Planet( float radius, float karmanLine );
+
+    /* Destructor */
     virtual ~Planet(void);
+
+private:
+
+    /* The planet's atmospheric shell */
+    Atmosphere _atmosphere;
+
+    /* Planet's radius */
+    float _r;
+
+    /* The upper limit of the planet's atmosphere */
+    float _karmanLine;
 };
 
 }
