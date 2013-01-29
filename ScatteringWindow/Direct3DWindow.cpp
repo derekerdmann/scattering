262dcd851dc9bf40563d5bf76387e8f34d283f03
Derek Erdmann <derek@derekerdmann.com>
2013-01-29 12:23:21 -0500

    Add the window's private construction helpers

--- ScatteringWindow/Direct3DWindow.cpp
+++ ScatteringWindow/Direct3DWindow.cpp
@@ -17,26 +17,29 @@ Direct3DWindow::~Direct3DWindow(void)
 }
 
 
-
 /* Main application loop */
 int Direct3DWindow::start() {
     return 0;
 }
+
 
 /* Handles resizing of the client window */
 void Direct3DWindow::onResize() {
 
 }
 
+
 /* Updates the items in the scene at each tick */
 void Direct3DWindow::updateScene() {
 
 }
 
+
 /* Renders the current scene to the window */
 void Direct3DWindow::drawScene() {
 
 }
+
 
 /* Handle Windows window messages */
 LRESULT Direct3DWindow::MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
@@ -47,4 +50,16 @@ LRESULT Direct3DWindow::MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lPara
 // Calculate the aspect ratio of the window
 float Direct3DWindow::aspectRatio() const {
 	return static_cast<float>(_width) / _height;
+}
+
+
+/* Initialize the main application window */
+void Direct3DWindow::createWindow() {
+
+}
+
+
+/* Initialize Direct3D objects */
+void Direct3DWindow::setupDirect3D() {
+
 }
