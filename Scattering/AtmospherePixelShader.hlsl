9dc4df3358a9d3163c47f6a81ef4aff0f9cd99bd
Derek Erdmann <derek@derekerdmann.com>
2013-02-10 14:53:43 -0500

    Start defining atmosphere constants and functions

--- Scattering/AtmospherePixelShader.hlsl
+++ Scattering/AtmospherePixelShader.hlsl
@@ -1,7 +1,43 @@
 
 #include "Structures.hlsli"
 
+cbuffer viewingData
+{
+    float3 cameraPosition;
+};
+
+// updated as the sun moves through the sky
+cbuffer sunDependent
+{
+    float sunAngle              // theta
+    float phaseFunctionResult   // F( theta, g )
+}
+
+// Should only be set once
+cbuffer atmosphericConstants 
+{
+    float3 sunIntensity             // I0 [R, G, B]
+    float3 attenuationCoefficient;  // beta [R, G, B]
+    float refractionIndex;          // n
+    float scaleHeight;              // H0
+};
+
+
+// Calculates optical depth along the vector S
+float opticalDepth( float3 S, float lambda )
+{
+    
+}
+
+// rho( h ) - Computes the density ratio at the specified height
+float densityRatio( float h )
+{
+    return exp( -h / scaleHeight );
+}
+
+
 float4 main( in VS_OUTPUT input ) : SV_TARGET
 {
     return input.Normal;
 }
+
